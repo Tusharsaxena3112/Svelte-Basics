0f8185046bd331b38bb87ceb43de159ffdbd895c
Tusharsaxena3112 <saxenatushar2001@gmail.com>
2021-06-30 20:11:12 +0530

    making todo list

--- svelteApp/src/App.svelte
+++ svelteApp/src/App.svelte
@@ -1,28 +1,9 @@
 <script>
-  // import Basics from "./1basics.svelte";
-  // import UserInput from "./2userInput.svelte";
-  // import Reactivity from "./3Reactivity.svelte";
-  // import Props from "./4Props.svelte";
-  // import Loops from "./5Loops.svelte";
-  // import AddItem from "./6AddingData.svelte";
-  import DeletetingItem from "./DeletetingItem.svelte";
-  // import DeleteItem from "./DeletetingItem.svelte";
-  const props = {
-    name: "Tushar",
-    age: 21,
-    gender: "male",
-    martialStatus: "single",
-  };
+  
 </script>
 
 <main>
-  <!-- <Basics/> -->
-  <!-- <UserInput/> -->
-  <!-- <Reactivity/> -->
-  <!-- <Props {...props}/> -->
-  <!-- <Loops/> -->
-  <!-- <AddItem/> -->
-  <DeletetingItem />
+  
 </main>
 
 <style>
